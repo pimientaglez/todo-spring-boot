784bab37bca108e387a0fb4d3b19004c4bc9e3b1
Ricardo Pimienta <pimientaglez@gmail.com>
2025-04-20 00:06:15 -0600

    Validate Attribute

--- src/main/java/com/pimientaglez/todo/task/Task.java
+++ src/main/java/com/pimientaglez/todo/task/Task.java
@@ -5,12 +5,14 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public record Task (
     @Id
     Integer id,
     @NotEmpty
     String  title,
+    @NotNull
     Status status,
     @Version
     Integer version 
