935b1ce3126694f1cdd5389cd74630c57db92c9c
Ricardo Pimienta <pimientaglez@gmail.com>
2025-04-23 14:54:19 -0600

    Allow CORS

---
+++ src/main/java/com/pimientaglez/todo/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.pimientaglez.todo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("http://localhost:3000") // React frontend origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(false); // Set to true if cookies or auth headers are needed
+    }
+}
